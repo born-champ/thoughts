bc8db9737e0c3d52d0539dc67a4777ba8d46897a
Abhishek Kumar Dubey <abhishek.dubey@geeksforgeeks.org>
2021-10-12 19:51:54 +0530

    updated ui and added pagination

--- thoughts/blog/views.py
+++ thoughts/blog/views.py
@@ -4,28 +4,51 @@ from django.shortcuts import HttpResponse
 from .models import Blog
 from django.views.decorators.csrf import csrf_exempt
 
-def home(request, *arg, **kwargs):
-	blogs = Blog.objects.all()
-	page = '''
-	<a href="/blog/"> Homepage</a>
-	<a href="/blog/post/"> Post</a>
-	<hr>
+page_count = 15
 
-	<h2>Blogs</h2><br>'''
-	for blog in blogs:
-		page  = page + "<h3>"+ str(blog.id)+"</h3>"
-		page  = page + "<h4>"+ str(blog.title)+"</h4>"
-		page  = page + "<p>"+ str(blog.body)+"</p>"
-		page  = page + "<hr>"
+default_page = '''
+			<br>
+			<nav margin>
+				&nbsp &nbsp &nbsp
+			  	
+			  	<a href="/blog/"> <b> Homepage </b> </a> 
+
+			  	&nbsp &nbsp | &nbsp &nbsp
+				
+				<a href="/blog/post/"> <b> Post </b> </a>
+			</nav>
+			<hr> 
+			'''
+
+def home(request, *arg, **kwargs):
+	req_data = request.GET
+	page = int(req_data.get('page')) if 'page' in req_data else 1
+
+	start = (page-1)*page_count
+	end = page_count*page
+
+	blogs = Blog.objects.all()[start:end]
+	page = default_page
+	page += '''
+		<h2>Blogs</h2><hr>
+	'''
+
+	if bool(blogs):
+		for blog in blogs:
+			page  += "<h4>"+ str(blog.title)+"</h4>"
+			page  += "<p>"+ str(blog.body)+"</p>"
+			page  += "<hr>"
+	else:
+		page+='''
+			<center><h3>Nothing on this page</h3><center>
+
+		'''
 
 	return HttpResponse(page)
 
 @csrf_exempt
 def post(request, *arg, **kwargs):
-	page = '''
-			<a href="/blog/"> Homepage</a>
-			<a href="/blog/post/"> Post</a>
-			<hr>'''
+	page = default_page
 	if request.method == "POST":
 		req_data = request.POST
 		data_list = ["title", "body"]
@@ -59,11 +82,11 @@ def post(request, *arg, **kwargs):
 		
 		page+='''
 			<center>
-				<h2>Fill data here</h2>
+				<h2>Your thoughts..</h2>
 				<form method= "post" action="/blog/post/">
-					Title: <input type = "text" name="title" required/><br>
-					Body: <textarea name= "body" required>
-					</textarea><br>
+					<textarea name= "title" rows="2" cols="50" placeholder="Title of you post" required></textarea><br>
+					<br>
+					<textarea name= "body" rows="10" cols="50" placeholder="Write your thought here"  required></textarea><br><br>
 					<button type = "submit">Post</buttom> 
 					</form>
 			</center>
