1f56952768375faec88894108dc973cc3a22c668
Abhishek Kumar Dubey <abhishek.dubey@geeksforgeeks.org>
2021-10-27 15:39:17 +0530

    added templates for frontend

--- thoughts/thoughts/settings.py
+++ thoughts/thoughts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,11 +52,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'thoughts.urls'
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_PATH, 'blog', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
